d2552bf49e4b390a6e5f72c3fa6a6f07e4c6f04a
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-01-25 16:05:14 +0530

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://local.rocket.co.in:8080/system/';
+$config['base_url'] = 'http://local.rocket.co.in:8081/test/system/';
 
 /*
 |--------------------------------------------------------------------------
